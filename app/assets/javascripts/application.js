c8252ce417076e3afa15983e8a56f2c3b51c9bd9
sachi <sachi@kamimuraandcompany.jp>
2018-08-25 15:39:00 +0900

    studied bootstrap and comment form create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery.matchHeight
+
+//matchHeight
+$(document).ready(function() {
+    $('.image-wrapper').matchHeight();
+});
